d3749618dd8ec3a9f1cbcbd74e4de67c8e715da3
ali3v <xalisoglu@gmail.com>
2025-12-15 15:18:57 +0400

    test

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sha256sum $1 > $1.sha256  # Generate the checksum file
-sha256sum -c $1.sha256    # Validate the file's checksum
+echo $(sha256sum $1 | cut -d ' ' -f1) == $2 && echo "$1: OK" || echo "$1: Mismatch"
