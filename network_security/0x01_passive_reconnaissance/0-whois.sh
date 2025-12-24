2d3b4286d59dcd2c08c20260363ad8fb849575ac
ali3v <xalisoglu@gmail.com>
2025-12-24 12:27:59 +0400

    test

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {
-    gsub(/^ /, "", $2);         # Dəyərin əvvəlindəki boşluğu silir
-    if ($1 ~ /Street/) {        # Əgər "Street" varsa
-        $2 = $2 " ";            # Dəyərin sonuna mütləq boşluq qoy (Checker bunu istəyir!)
-    }
-    if ($1 ~ /Ext/) {           # Əgər "Ext" varsa
-        $1 = $1 ":";            # Sahə adının sonuna : qoy
-    }
-    print $1 "," $2             # CSV formatında çap et
-}'
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {gsub(/^ /, "", $2); if ($1 ~ /Street/) $2 = $2 " "; if ($1 ~ /Ext/) $1 = $1 ":"; print $1 "," $2}'
