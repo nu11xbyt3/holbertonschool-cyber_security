70df0401c11f5a6074af346141562daef817c625
ali3v <xalisoglu@gmail.com>
2025-12-24 13:13:45 +0400

    test

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent | tee /dev/tty | while read d; do ip=$(dig +short "$d" | grep '^[0-9]' | head -1); [[ -n "$ip" ]] && echo "$d,$ip"; done > "$1.txt"
+subfinder -d $1 -silent -nW -oI -o $1.txt
